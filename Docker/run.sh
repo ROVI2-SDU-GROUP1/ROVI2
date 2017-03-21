2cd4982bb7b3137fc49a9fc5d4daf92f81b5ef09
Stefan Ravn van Overeem <stefan@stefanrvo.dk>
2017-03-21 17:40:53 +0100

    Updates to run.sh

--- Docker/run.sh
+++ Docker/run.sh
@@ -6,9 +6,15 @@ source ROVI2/code/devel/setup.sh
 #sleep a bit to be safe
 # sleep 3
 #Run demo program.
-sed -i "s/192.168.100.4/$UR_IP/"   ROVI2/code/src/caros/hwcomponents/caros_universalrobot/launch/caros_universalrobot_param.xml
-sed -i "s/192.168.100.1/$CONTAINER_IP/" ROVI2/code/src/caros/hwcomponents/caros_universalrobot/launch/caros_universalrobot_param.xml
+if [ $UR_IP = "192.168.100.1" ]; then
+	sed -i "s/\"192.168.100.1\"/\"$CONTAINER_IP\"/" ROVI2/code/src/caros/hwcomponents/caros_universalrobot/launch/caros_universalrobot_param.xml
+	sed -i "s/\"192.168.100.4\"/\"$UR_IP\"/"   ROVI2/code/src/caros/hwcomponents/caros_universalrobot/launch/caros_universalrobot_param.xml
+else
+	sed -i "s/\"192.168.100.4\"/\"$UR_IP\"/"   ROVI2/code/src/caros/hwcomponents/caros_universalrobot/launch/caros_universalrobot_param.xml
+	sed -i "s/\"192.168.100.1\"/\"$CONTAINER_IP\"/" ROVI2/code/src/caros/hwcomponents/caros_universalrobot/launch/caros_universalrobot_param.xml
+fi
 
 #roslaunch caros_universalrobot simple_demo_using_move_ptp.test device_ip:=$UR_IP callback_ip:=$CONTAINER_IP
-roslaunch caros_universalrobot simple_demo_using_move_ptp.test
+roslaunch ROVI2_Development start_hw_node.launch &
+rosrun rovisquaremover node
 
