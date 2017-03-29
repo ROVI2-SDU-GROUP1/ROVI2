f0239a33ed8a96d830d64125d04dfe713f23891c
Stefan Ravn van Overeem <stefan@stefanrvo.dk>
2017-03-29 23:34:05 +0200

    Updates to kalman estimater. Mostly just missing update step (and testing...)

--- code/src/rovi2_development/headers/kalman_estimator_node.hpp
+++ code/src/rovi2_development/headers/kalman_estimator_node.hpp
@@ -1,8 +1,7 @@
 #pragma once
 #include <eigen3/Eigen/Eigen>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
-
-
+//Some inspiration can be gained from http://www.image.esys.tsukuba.ac.jp/~kitahara/data/pubs/200712_PCM2007-ishii.pdf
 class Kalman_Estimator
 {
     public:
@@ -14,4 +13,15 @@ class Kalman_Estimator
         ros::Subscriber sub;
         ros::Publisher pub;
         tf2::Stamped<Eigen::Vector3d> prev_points[3]; //The three previosly recieved points
+        Eigen::VectorXd cur_state;
+        Eigen::Matrix<double, 9, 9> cur_covariance;
+        Eigen::Matrix<double, 9, 9> transition_matrix;
+        Eigen::Matrix<double, 9, 9> measurement_matrix;
+
+        ros::Time t0;
+        uint64_t position_count = 0;
+        void do_predict_step();
+        void do_update_step(Eigen::VectorXd measured_state);
+        void update_transition_matrix(double time_step);
+
 };
--- code/src/rovi2_development/src/kalman_estimator_node.cpp
+++ code/src/rovi2_development/src/kalman_estimator_node.cpp
@@ -24,36 +24,110 @@ geometry_msgs::Point vector3d_to_point(Eigen::Vector3d &in)
     return out;
 }
 
+void Kalman_Estimator::do_predict_step()
+{
+    //Perform a state estimate
+    this->cur_state = this->transition_matrix * this->cur_state;
+    //Perform a covariance estimate
+    this->cur_covariance = this->transition_matrix * this->cur_covariance;
+}
+
+void Kalman_Estimator::do_update_step(__attribute__((unused)) Eigen::VectorXd measured_state)
+{
+    //lulz, just skip for now.
+}
+
+void Kalman_Estimator::update_transition_matrix(double time_step)
+{
+    //at2 means 0.5 * dt * dt
+    //The matrix will be:
+    //[1,   0,   0,  dt,   0,   0, at2,   0,   0]
+    //[0,   1,   0,   0,  dt,   0,   0, at2,   0]
+    //[0,   0,   1,   0,   0,  dt,   0,   0, at2]
+    //[0,   0,   0,   1,   0,   0,  dt,   0,   0]
+    //[0,   0,   0,   0,   1,   0,   0,  dt,   0]
+    //[0,   0,   0,   0,   0,   1,   0,   0,  dt]
+    //[0,   0,   0,   0,   0,   0,   1,   0,   0]
+    //[0,   0,   0,   0,   0,   0,   0,   1,   0]
+    //[0,   0,   0,   0,   0,   0,   0,   0,   1]
+
+    //Set the dt fields
+    uint8_t y = 0;
+    for(uint8_t x = 3; x <  this->transition_matrix.cols(); x++, y++)
+    {
+        this->transition_matrix(y, x) = time_step;
+    }
+    //Set the at2 fields
+    y = 0;
+    for(uint8_t x = 6; x <  this->transition_matrix.cols(); x++, y++)
+    {
+        this->transition_matrix(y, x) = this->cur_state(x) * 0.5 * time_step * time_step ;
+    }
+
+}
 
 void Kalman_Estimator::pose_callback( __attribute__((unused)) const geometry_msgs::PointStamped::ConstPtr &msg)
 {
+  position_count++;
+
   //Propegate the array of last positions
   geometry_msgs::PointStamped this_pt = *msg;
   this->prev_points[2] = std::move(this->prev_points[1]);
   this->prev_points[1] = std::move(this->prev_points[0]);
   this->prev_points[0] = pointstamped_to_vector3d(this_pt);
+  if(position_count < 3) return; //We don't have enough positions yet to estimate the parameters.
+                                 //We could probably do a fallback after two samples to some default acceleration parameters
 
+  //Compute speeds and accelerations
   Eigen::Vector3d cur_speed = (this->prev_points[1] - this->prev_points[0]) / (this->prev_points[1].stamp_ - this->prev_points[0].stamp_).toSec();
   Eigen::Vector3d last_speed = (this->prev_points[2] - this->prev_points[1]) / (this->prev_points[2].stamp_ - this->prev_points[1].stamp_).toSec();
   Eigen::Vector3d acc = (last_speed - cur_speed) / (this->prev_points[1].stamp_ - this->prev_points[0].stamp_).toSec();;
   Eigen::Vector3d pos = this->prev_points[0];
+  Eigen::VectorXd measured_state(9);
+  measured_state(0) = pos(0);
+  measured_state(1) = pos(1);
+  measured_state(2) = pos(2);
+  measured_state(3) = acc(0);
+  measured_state(4) = acc(1);
+  measured_state(5) = acc(2);
+  measured_state(6) = cur_speed(0);
+  measured_state(7) = cur_speed(1);
+  measured_state(8) = cur_speed(2);
 
 
+  this->update_transition_matrix((this->prev_points[1].stamp_ - this->prev_points[0].stamp_).toSec());
+  this->do_predict_step();
+  this->do_update_step(measured_state);
   //  Do the parameter estimation // TODO
   rovi2_development::Trajectory3D traj;
   traj.header.stamp = this_pt.header.stamp;
-  traj.acc = vector3d_to_point(acc);
-  traj.vel = vector3d_to_point(cur_speed);
-  traj.pos = vector3d_to_point(pos);
-  traj.t0 = this_pt.header.stamp;
+  //traj.acc = vector3d_to_point(acc);
+  //traj.vel = vector3d_to_point(cur_speed);
+  //traj.pos = vector3d_to_point(pos);
+  //traj.t0 = this_pt.header.stamp;
   this->pub.publish(traj);
-
 }
 
 Kalman_Estimator::Kalman_Estimator()
 : sub(nh.subscribe<geometry_msgs::PointStamped>("/pose/3d",1, &Kalman_Estimator::pose_callback, this)),
-  pub(nh.advertise<rovi2_development::Trajectory3D>("/pose/parameter",1))
+  pub(nh.advertise<rovi2_development::Trajectory3D>("/pose/parameter",1)),
+  cur_state(9)
 {
+    //Initialise current state as 0 for all.
+    this->cur_state.setZero();
+
+    //Initialise co-variance matrix.
+    //We don't know where the ball is at all, so initialise extremly high)
+    for(uint8_t i = 0; i < this->cur_covariance.rows(); i++)
+        this->cur_covariance(i, i) = 99999.;
+
+    //Set the measurement matrix. This is easy, it's just identity.
+    this->measurement_matrix = Eigen::MatrixXd::Identity(this->measurement_matrix.rows(), this->measurement_matrix.cols());
+
+    //Now the transistion matrix. It's more complex.
+    //Here we just set it as identity. The other parameters are dynamic, so we have to change them each time anyway.
+    //See update_transistion_matrix for details.
+    this->transition_matrix = Eigen::MatrixXd::Identity(this->transition_matrix.rows(), this->transition_matrix.cols());
 
 }
 
