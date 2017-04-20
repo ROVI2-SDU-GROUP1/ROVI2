3f8d89fdc14997da0d2cf3721b6792c9c85fbcd8
RasmusKS <rasmuskstagsted@live.dk>
2017-04-20 14:55:29 +0200

    Fixed stuf before launch file setup.

--- code/src/rovi2_development/src/image_detector_node.cpp
+++ code/src/rovi2_development/src/image_detector_node.cpp
@@ -7,28 +7,152 @@
 #include <sensor_msgs/Image.h>
 #include <geometry_msgs/PointStamped.h>
 
+int mx = 0;
+int my = 0;
+
+void mouseCallback(int event, int x, int y, int flags, void* userdata){
+  std::cout << x << "," << y << std::endl;
+}
+
 ros::Subscriber sub;
 ros::Publisher pub;
+ros::Publisher pub1;
+
+void closing(cv::Mat &src){
+  cv::Mat dst;
+
+  int erosion_size_dilate = 10;
+  cv::Mat elementDilate = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(2 * erosion_size_dilate + 1, 2 * erosion_size_dilate + 1), cv::Point(erosion_size_dilate, erosion_size_dilate));
+  cv::dilate(src, dst, elementDilate);
+
+  int erosion_size_erode = 7;
+  cv::Mat elementErode = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(2 * erosion_size_erode + 1, 2 * erosion_size_erode + 1), cv::Point(erosion_size_erode, erosion_size_erode));
+  cv::erode(dst, src, elementErode);
+}
+
+void opening(cv::Mat &src){
+  cv::Mat dst;
+
+  int erosion_size_erode = 7;
+  cv::Mat elementErode = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(2 * erosion_size_erode + 1, 2 * erosion_size_erode + 1), cv::Point(erosion_size_erode, erosion_size_erode));
+  cv::erode(src, dst, elementErode);
+
+  int erosion_size_dilate = 10;
+  cv::Mat elementDilate = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(2 * erosion_size_dilate + 1, 2 * erosion_size_dilate + 1), cv::Point(erosion_size_dilate, erosion_size_dilate));
+  cv::dilate(dst, src, elementDilate);
+}
+
+int constrain(int x, int min, int max){
+  if (x < min) return min;
+  if (max < x) return max;
+  return x;
+}
+
+int abs(int x){
+  if (x < 0) return -x;
+  return x;
+}
+
+// Adaptive filter
+void adaptive(cv::Mat &source, cv::Mat &dist, cv::Scalar color, int threshold){
+  dist.create(source.rows, source.cols, CV_8UC1);
+  for (int i = 0; i < source.rows; i++){
+    for (int j = 0; j < source.cols; j++){
+      for (int k = 0; k < 3; k++){
+        int temp = source.at<cv::Vec3b>(i, j)[k] - color[k];
+        dist.at<char>(i, j) = constrain(abs(temp), 0, 255);
+      }
+    }
+  }
+  /*int halfMask = (mask.cols - 1) / 2;
+  for (int y = halfMask; y < source.rows - halfMask; y++){
+    for (int x = halfMask; x < source.cols - halfMask; x++){
+      int temp = 0;
+      for (int i = 0; i <= halfMask * 2; i++){
+        for (int j = 0; j <= halfMask * 2; j++){
+          for (int c = 0; c < 3; c++){
+            temp += mask.at<cv::Vec3b>(i, j)[c] * (color[c] - source.at<cv::Vec3b>(y + i - halfMask, x + j - halfMask)[c]);
+          }
+        }
+      }
+      if (temp / 256 < threshold) dist.at<char>(y, x) = 255;
+      else dist.at<char>(y, x) = 0;
+    }
+  }*/
+}
 
 // state subscriber
 void find2DPose(const sensor_msgs::Image::ConstPtr& msg){
   cv_bridge::CvImagePtr cv_ptr;
 
-  try {cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);}
-  catch (cv_bridge::Exception& e) {
+  try{
+    cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+  }
+  catch (cv_bridge::Exception& e){
     ROS_ERROR("cv_bridge exception: %s", e.what());
     return;
   }
 
-  cv::Mat image = cv_ptr->image.clone();
+  cv::Mat imageBGR = cv_ptr->image.clone();
+  cv::Mat imageHSV;
+  cv::cvtColor(imageBGR, imageHSV, CV_BGR2HSV);
 
-  //cv::imshow("test", image);
-  //cv::waitKey(1);
 
-  // Do image detection // TODO
+/*
+  cv::Mat threshold;
+  adaptive(imageHSV, threshold, cv::Scalar(0, 255, 255), 10);
+
+
+  std::cout << imageHSV.at<cv::Vec3b>(my, mx) << std::endl;
+
+  */
+  cv::Mat thresholdLow;
+  cv::Mat thresholdHigh;
+  cv::inRange(imageHSV, cv::Scalar(0, 100, 50), cv::Scalar(10, 255, 255), thresholdLow);
+  cv::inRange(imageHSV, cv::Scalar(215, 100, 50), cv::Scalar(255, 255, 255), thresholdHigh);
+
+  cv::Mat threshold = thresholdLow + thresholdHigh;
+
+  opening(threshold);
+  closing(threshold);
+
+
+	std::vector<std::vector<cv::Point> > cont;
+	std::vector<cv::Vec4i> hier;
+	cv::findContours(threshold, cont, hier, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );
 
   geometry_msgs::PointStamped point;
-	pub.publish(point);
+
+  if (!cont.size()){
+    //cv::imshow("t2", imageBGR);
+    //cv::waitKey(1);
+    pub.publish(point);
+    return;
+  }
+
+	cv::Moments moments = cv::moments(cont[0], false);
+	cv::Point2f imgCoord = cv::Point2f(moments.m10 / moments.m00, moments.m01 / moments.m00);
+
+  point.point.x = imgCoord.x;
+  point.point.y = imgCoord.y;
+  point.header.stamp = msg->header.stamp;
+
+  cv::circle(imageBGR, imgCoord, 10, cv::Scalar(0, 255, 0), 2);
+
+  /*
+  cv::imshow("t2", imageBGR);
+  cv::waitKey(1);
+  cv::imshow("t1", threshold);
+  cv::waitKey(1);
+  */
+
+  //cv::setMouseCallback("t2", mouseCallback, NULL);
+
+
+  //
+  pub.publish(point);
+  point.point.x = point.point.x - 100;
+  pub1.publish(point);
 }
 
 int main(int argc, char **argv){
@@ -39,6 +163,7 @@ int main(int argc, char **argv){
 
   sub = nh.subscribe<sensor_msgs::Image>("/camera/left/image_raw", 1, find2DPose);
   pub = nh.advertise<geometry_msgs::PointStamped>("/pose/2d_left", 1);
+  pub1 = nh.advertise<geometry_msgs::PointStamped>("/pose/2d_right", 1);
 
   ros::Time last = ros::Time::now();
 
--- code/src/rovi2_development/src/stereo_node.cpp
+++ code/src/rovi2_development/src/stereo_node.cpp
@@ -1,41 +1,162 @@
 #include <mutex>
 #include <geometry_msgs/PointStamped.h>
 #include <ros/ros.h>
+#include <fstream>
+#include <eigen3/Eigen/Dense>
+#include <eigen3/Eigen/Core>
+#include <eigen3/Eigen/SVD>
+
+ros::Time leftTime;
+ros::Time rightTime;
 
 ros::Subscriber sub2DLeft;
 ros::Subscriber sub2DRight;
+
 ros::Publisher pub3D;
 
 std::mutex mutex;
-char poses = 0;
 
-geometry_msgs::PointStamped leftPose;
-geometry_msgs::PointStamped rightPose;
+geometry_msgs::PointStamped pose2DLeft;
+geometry_msgs::PointStamped pose2DRight;
+
+template<typename _Matrix_Type_>_Matrix_Type_ pseudoInverse(const _Matrix_Type_ &a, double epsilon = std::numeric_limits<double>::epsilon())
+{
+    Eigen::JacobiSVD< _Matrix_Type_ > svd(a ,Eigen::ComputeThinU | Eigen::ComputeThinV);
+    double tolerance = epsilon * std::max(a.cols(), a.rows()) *svd.singularValues().array().abs()(0);
+    return svd.matrixV() *  (svd.singularValues().array().abs() > tolerance).select(svd.singularValues().array().inverse(), 0).matrix().asDiagonal() * svd.matrixU().adjoint();
+}
+
+struct Intrinsic{
+  int width;
+  int height;
+  Eigen::MatrixXd intrinsic;
+  Eigen::MatrixXd distortion;
+  Eigen::MatrixXd rotation;
+  Eigen::MatrixXd translation;
+
+  Eigen::MatrixXd transformation;
+  Eigen::MatrixXd P;
+  Eigen::MatrixXd PX;
+  Eigen::MatrixXd px;
+  Eigen::MatrixXd center;
+};
+
+Intrinsic calL;
+Intrinsic calR;
+
+Intrinsic loadCalibration(std::string fileName, int index){
+  Intrinsic cal;
+  std::ifstream file(fileName);
+  if (!file.is_open());
+  std::string line;
+  while (getline(file, line)){
+    std::cout << line << std::endl;
+  }
+
+  cal.intrinsic.resize(3, 3);
+  cal.intrinsic.row(0) << 1280, 0, 512;
+  cal.intrinsic.row(1) << 0, 1280, 384;
+  cal.intrinsic.row(2) << 0, 0, 1;
+
+  cal.rotation.resize(3, 3);
+  cal.rotation.row(0) << 0, 1, 0;
+  cal.rotation.row(1) << 0, 0, -1;
+  cal.rotation.row(2) << -1, 0, 0;
+
+  cal.translation.resize(3, 1);
+  if (!index) cal.translation.col(0) << -0.373460680246, 0.724578678608, 10.2876386642;
+  else cal.translation.col(0) << 0.373460680246, 0.724578678608, 10.2876386642;
+
+  Eigen::MatrixXd temp1;
+  Eigen::MatrixXd temp2;
+
+  // Transformation
+  temp1.resize(3, 4);
+  temp2.resize(1, 4);
+  temp1 << cal.rotation, cal.translation;
+  temp2.row(0) << 0, 0, 0, 1;
+  cal.transformation.resize(4, 4);
+  cal.transformation << temp1, temp2;
+
+  // P
+  temp1.resize(3, 4);
+  temp2.resize(3, 1);
+  temp2.col(0) << 0, 0, 0;
+  temp1 << cal.intrinsic, temp2;
+  cal.P = temp1 * cal.transformation;
+
+  // PX
+  cal.PX = cal.P.block(0, 0, 3, 3);
+
+  // px
+  cal.px = cal.P.block(0, 3, 3, 1);
+
+  // center
+  temp1.resize(3, 1);
+  temp1 = -cal.rotation.inverse() * cal.translation;
+  cal.center.resize(4, 1);
+  cal.center << temp1, 1;
+
+  return cal;
+}
 
 void calc3DPose(){
-  if (poses < 2) return;
+  std::cout << "Calc 3d pos!" << std::endl;
 
   geometry_msgs::PointStamped pose3D;
 
-  // TODO: do some vision
+  Eigen::MatrixXd A(4, 3);
+  A.row(0) = -pose2DLeft.point.x * calL.PX.row(2) + calL.PX.row(0);
+  A.row(1) = -pose2DLeft.point.y * calL.PX.row(2) + calL.PX.row(1);
+  A.row(2) = -pose2DRight.point.x * calR.PX.row(2) + calR.PX.row(0);
+  A.row(3) = -pose2DRight.point.y * calR.PX.row(2) + calR.PX.row(1);
+
+  Eigen::MatrixXd B(4, 1);
+  B.row(0) = pose2DLeft.point.x * calL.px.row(2) - calL.px.row(0);
+  B.row(1) = pose2DLeft.point.y * calL.px.row(2) - calL.px.row(1);
+  B.row(2) = pose2DRight.point.x * calR.px.row(2) - calR.px.row(0);
+  B.row(3) = pose2DRight.point.y * calR.px.row(2) - calR.px.row(1);
+
+
+  std::cout << "PX left:" << std::endl;
+  std::cout << calL.PX << std::endl;
+  std::cout << "px left:" << std::endl;
+  std::cout << calL.px << std::endl;
+  std::cout << "PX right:" << std::endl;
+  std::cout << calR.PX << std::endl;
+  std::cout << "px right:" << std::endl;
+  std::cout << calR.px << std::endl;
+  std::cout << "A:" << std::endl;
+  std::cout << A << std::endl;
+  std::cout << "B:" << std::endl;
+  std::cout << B << std::endl;
+
+  Eigen::MatrixXd x(3, 1);
+  x = (pseudoInverse(A) * A).inverse() * pseudoInverse(A) * B;
+
+  pose3D.point.x = x(0, 0);
+  pose3D.point.y = x(1, 0);
+  pose3D.point.z = x(2, 0);
+
+  pose3D.header.stamp = pose2DLeft.header.stamp;
 
   pub3D.publish(pose3D);
 }
 
 void receiveLeftImage(const geometry_msgs::PointStamped::ConstPtr &msg){
-  leftPose = *msg;
   mutex.lock();
-  poses++;
+  pose2DLeft = *msg;
+  leftTime = msg->header.stamp;
+  if (leftTime == rightTime) calc3DPose();
   mutex.unlock();
-  calc3DPose();
 }
 
 void receiveRightImage(const geometry_msgs::PointStamped::ConstPtr &msg){
-  rightPose = *msg;
   mutex.lock();
-  poses++;
+  pose2DRight = *msg;
+  rightTime = msg->header.stamp;
+  if (leftTime == rightTime) calc3DPose();
   mutex.unlock();
-  calc3DPose();
 }
 
 int main(int argc, char **argv){
@@ -43,6 +164,9 @@ int main(int argc, char **argv){
     ros::init(argc, argv, "stereo");
   	ros::NodeHandle nh;
   	ros::Rate rate(20);
+
+    calL = loadCalibration("calibration.txt", 0);
+    calR = loadCalibration("calibration.txt", 1);
 
     sub2DLeft = nh.subscribe<geometry_msgs::PointStamped>("/pose/2d_left", 1, receiveLeftImage);
     sub2DRight = nh.subscribe<geometry_msgs::PointStamped>("/pose/2d_right", 1, receiveRightImage);
