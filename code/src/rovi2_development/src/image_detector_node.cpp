a44c4b06c10dd9ccc1db05cc813ea86d8faf8935
Mathias Neerup <manee12@student.sdu.dk>
2017-05-04 12:44:14 +0200

    Dector_node undistorts images, reads calibration parameters from files in calibration/

--- code/src/rovi2_development/src/image_detector_node.cpp
+++ code/src/rovi2_development/src/image_detector_node.cpp
@@ -7,9 +7,15 @@
 #include <sensor_msgs/Image.h>
 #include <geometry_msgs/PointStamped.h>
 #include <cassert>
+#include "yaml-cpp/yaml.h"
+#include <fstream>
 
 int mx = 0;
 int my = 0;
+
+cv::Mat *cameraMatrix;
+cv::Mat *distCoeffs;
+
 
 ros::Subscriber image_sub;
 ros::Publisher point_pub;
@@ -38,43 +44,17 @@ void opening(cv::Mat &src){
   cv::dilate(dst, src, elementDilate);
 }
 
-int constrain(int x, int min, int max){
-  if (x < min) return min;
-  if (max < x) return max;
-  return x;
-}
-
 int abs(int x){
   if (x < 0) return -x;
   return x;
 }
 
-// Adaptive filter
-void adaptive(cv::Mat &source, cv::Mat &dist, cv::Scalar color, int threshold){
-  dist.create(source.rows, source.cols, CV_8UC1);
-  for (int i = 0; i < source.rows; i++){
-    for (int j = 0; j < source.cols; j++){
-      for (int k = 0; k < 3; k++){
-        int temp = source.at<cv::Vec3b>(i, j)[k] - color[k];
-        dist.at<char>(i, j) = constrain(abs(temp), 0, 255);
-      }
-    }
-  }
-  /*int halfMask = (mask.cols - 1) / 2;
-  for (int y = halfMask; y < source.rows - halfMask; y++){
-    for (int x = halfMask; x < source.cols - halfMask; x++){
-      int temp = 0;
-      for (int i = 0; i <= halfMask * 2; i++){
-        for (int j = 0; j <= halfMask * 2; j++){
-          for (int c = 0; c < 3; c++){
-            temp += mask.at<cv::Vec3b>(i, j)[c] * (color[c] - source.at<cv::Vec3b>(y + i - halfMask, x + j - halfMask)[c]);
-          }
-        }
-      }
-      if (temp / 256 < threshold) dist.at<char>(y, x) = 255;
-      else dist.at<char>(y, x) = 0;
-    }
-  }*/
+
+cv::Mat Undistored(cv::Mat input){
+  cv::Mat undistorted;
+  cv::undistort(input, undistorted, *cameraMatrix, *distCoeffs);
+
+  return undistorted;
 }
 
 // state subscriber
@@ -89,9 +69,13 @@ void find2DPose(const sensor_msgs::Image::ConstPtr& msg){
     return;
   }
 
+
   cv::Mat imageBGR = cv_ptr->image.clone();
+  cv::Mat imageUndistorted = Undistored(imageBGR);
+
+
   cv::Mat imageHSV;
-  cv::cvtColor(imageBGR, imageHSV, CV_BGR2HSV);
+  cv::cvtColor(imageUndistorted, imageHSV, CV_BGR2HSV);
 
 
 /*
@@ -103,8 +87,8 @@ void find2DPose(const sensor_msgs::Image::ConstPtr& msg){
 */
   cv::Mat thresholdLow;
   cv::Mat thresholdHigh;
-  cv::inRange(imageHSV, cv::Scalar(0, 220, 50), cv::Scalar(10, 255, 255), thresholdLow);
-  cv::inRange(imageHSV, cv::Scalar(215, 220, 50), cv::Scalar(255, 255, 255), thresholdHigh);
+  cv::inRange(imageHSV, cv::Scalar(0, 150, 50), cv::Scalar(10, 255, 255), thresholdLow);
+  cv::inRange(imageHSV, cv::Scalar(215, 150, 50), cv::Scalar(255, 255, 255), thresholdHigh);
 
   cv::Mat threshold = thresholdLow + thresholdHigh;
 
@@ -132,26 +116,29 @@ void find2DPose(const sensor_msgs::Image::ConstPtr& msg){
   point.point.y = imgCoord.y;
   point.header.stamp = msg->header.stamp;
 
-  cv::circle(imageBGR, imgCoord, 10, cv::Scalar(0, 255, 0), 2);
+  cv::circle(imageUndistorted, imgCoord, 10, cv::Scalar(0, 255, 0), 2);
 
-/*
-  cv::imshow("t2", imageBGR);
+
+//  cv::imshow("Distorted", imageBGR);
+//  cv::imshow("Undistorted", imageUndistorted);
+//  cv::imshow("t1", threshold);
+
   cv::waitKey(1);
-  cv::imshow("t1", threshold);
-  cv::waitKey(1);
-*/
-
-  //cv::setMouseCallback("t2", mouseCallback, NULL);
 
 
-  //
   point_pub.publish(point);
 }
 
-int main(int argc, char **argv){
+bool is_file_exist(std::string fileName){
+    std::ifstream infile(fileName.c_str());
+    return infile.good();
+}
 
+int main(int argc, char **argv){
   std::string image_sub_name;
   std::string point_pub_name;
+  std::string yaml_path;
+
 
   ros::init(argc, argv, "image_detection");
 	ros::NodeHandle nh("~");
@@ -159,6 +146,35 @@ int main(int argc, char **argv){
 
   nh.param<std::string>("image_sub", image_sub_name, "/camera/left/image_raw");
   nh.param<std::string>("point_pub", point_pub_name, "/pose/2d_left");
+  nh.param<std::string>("calibration_yaml_path", yaml_path, "default.yaml");
+
+  std::string abs_yaml_path = std::string(CALIBRATION_DIR) + yaml_path;
+  std::cout << "Loading calib: " << abs_yaml_path << std::endl;
+  if(!is_file_exist(abs_yaml_path)){
+    ROS_WARN("File: %s does not exist\n", abs_yaml_path.c_str());
+    exit(1);
+  }
+
+  // Read yaml parameters
+  YAML::Node calibration_yaml = YAML::LoadFile(abs_yaml_path);
+  YAML::Node camera_matrix = calibration_yaml["camera_matrix"];
+  YAML::Node distortion_coefficients = calibration_yaml["distortion_coefficients"];
+
+  cameraMatrix = new cv::Mat(3,3, CV_64FC1);
+  distCoeffs = new cv::Mat(1,5, CV_64FC1);
+
+  for(unsigned int i = 0; i < 3; i++){
+    for(unsigned int j = 0; j < 3; j++){
+      cameraMatrix->at<double>(i,j) = camera_matrix["data"][i*3+j].as<double>();
+    }
+  }
+
+  //cv::Mat distCoeffs;
+  for(unsigned int i = 0; i < distortion_coefficients["data"].size();i++){
+    distCoeffs->at<double>(i) = distortion_coefficients["data"][i].as<double>();
+  }
+
+
 
   image_sub = nh.subscribe<sensor_msgs::Image>(image_sub_name, 1, find2DPose);
   point_pub = nh.advertise<geometry_msgs::PointStamped>(point_pub_name, 1);
