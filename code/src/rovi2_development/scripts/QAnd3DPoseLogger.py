3a3188931d78ab688f83da94caace1d917d03f39
Mathias Neerup <manee12@student.sdu.dk>
2017-05-09 11:11:24 +0200

    Fixed topic in logger-node

--- code/src/rovi2_development/scripts/QAnd3DPoseLogger.py
+++ code/src/rovi2_development/scripts/QAnd3DPoseLogger.py
@@ -11,9 +11,11 @@ import time
 timestr = time.strftime("%Y%m%d-%H%M%S")
 f = open('/tmp/' + timestr + ".csv", 'w')
 
-def callback(Q, Pose):
+def callback(Point, Robot):
 	print "Combined Q and pose";
-	f.write(str(Q.q.data) + "," + str(Pose.transform.translation.x) + "," + str(Pose.transform.translation.y) + "," + str(Pose.transform.translation.z) + "\n");
+	f.write(str(Point.point.x) + "," + str(Point.point.y) + "," + str(Point.point.z) \
+	 + "," + str(Robot.transform.translation.x) + "," + str(Robot.transform.translation.y) \
+	 + "," + str(Robot.transform.translation.z) + "\n");
 	f.flush();
 
 def myhook():
@@ -29,6 +31,6 @@ param_sub_pose_3d = rospy.get_param('~sub_3d_point', '/pose/3d')
 sub_transform = message_filters.Subscriber(param_sub_robot_state, TransformStamped)
 sub_3d_point = message_filters.Subscriber(param_sub_pose_3d, PointStamped)
 
-ts = message_filters.TimeSynchronizer([sub_transform, sub_3d_point], 10)
+ts = message_filters.TimeSynchronizer([sub_3d_point,sub_transform], 10)
 ts.registerCallback(callback)
 rospy.spin()
