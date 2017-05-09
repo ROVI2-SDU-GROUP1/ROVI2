d14f056a3ec666dd909301505cdc904320646adb
Mathias Neerup <manee12@student.sdu.dk>
2017-05-09 11:15:29 +0200

    Reads csv from hand_to_eye-calibration script

--- matlab/find_marker_cam.m
+++ matlab/find_marker_cam.m
@@ -1,18 +1,27 @@
 clc; clear;
-robot_points = [];
-camera_points = [];
 
+data = csvread('~/TEST_hand_to_eye.csv');
+
+
+
+robot_points = data(:,1:3);
+camera_points = data(:,4:6);
+%%
+scatter3(camera_points(:,1),camera_points(:,2), camera_points(:,3));
+%%
+clc; 
 assert( length(robot_points) == length(camera_points) );
 
 % Calculate centroids
  mu_base = sum(robot_points)/length(robot_points);
- mu_cam = sum(camera_points)/length(camera_points);
+ mu_camera = sum(camera_points)/length(camera_points);
 
- H = [];
+ H = zeros(3);
  
  for i = 1:length(robot_points)
-    H = H + (robot_points(i) - mu_base)*(camera_points(i) - my_camera)';
+    H = H + (robot_points(i,:) - mu_base)'*(camera_points(i,:) - mu_camera);
  end
+ 
  [U,S,V] = svd(H);
  
  R = V*U';
@@ -20,4 +29,6 @@ assert( length(robot_points) == length(camera_points) );
  if det(R) < 0
      R(:,3) = R(:,3)*-1;
  end
- P = - R*mu_base + mu_cam
+ 
+ P = - R*mu_base' + mu_camera';
+ norm(P)
