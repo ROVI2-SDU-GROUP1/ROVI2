6b0db0ab18aa1b875bc33bdf433e14b8f401c6f0
Mathias Neerup <manee12@student.sdu.dk>
2017-05-14 23:17:38 +0200

    Save and read Hand-to-eye(hte) calibration to/from a file.yaml

--- code/src/rovi2_development/src/frame_trans.cpp
+++ code/src/rovi2_development/src/frame_trans.cpp
@@ -1,5 +1,10 @@
 #include "rwlibs/algorithms/PointPairsRegistration.hpp"
-#include <fstream>
+#include "YAMLCalibration/YAMLCalibration.hpp"
+#include <opencv2/core/mat.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+
 
 int main(){
   std::vector< rwlibs::algorithms::PointPairsRegistration::PointPair > pointPairs;
@@ -39,19 +44,36 @@ int main(){
 
         std::cout << robot_vec << std::endl;
 
-        pointPairs.push_back(std::make_pair(robot_vec,cam_vec));
+        pointPairs.push_back(std::make_pair(cam_vec,robot_vec));
     }
 
   std::cout << "Num of pointPairs: " << pointPairs.size() << std::endl;
-  rw::math::Transform3D<> Trans_camera_in_base =  rwlibs::algorithms::PointPairsRegistration::pointPairRegistrationSVD(pointPairs);
+  rw::math::Transform3D<> Trans_camera_in_base = rwlibs::algorithms::PointPairsRegistration::pointPairRegistrationSVD(pointPairs);
+
+  YAMLCalibration::WriteToYaml(std::string(CALIBRATION_DIR) + "hte.yaml", Trans_camera_in_base);
+
+  YAMLCalibration hte_calibration(std::string(CALIBRATION_DIR) + "hte.yaml");
+  cv::Mat translation_vector = hte_calibration.get_translation_vector();
+  cv::Mat rotation_matrix = hte_calibration.get_rotation_matrix();
+
+  std::cout << translation_vector << std::endl;
+  std::cout << rotation_matrix << std::endl;
 
 
-  //rw::math::Vector3D<> P1(-0.317222072079,-0.487509135996,-1.85257493885);
-  rw::math::Vector3D<> P1(0.181131055917,0.37173324503,2.15828502449);
-  std::cout << Trans_camera_in_base.P().norm2() << std::endl;
+  rw::math::Vector3D<> P1(0.181131055917,0.37173324503,2.15828502449); // <- right side of base-plate
+  rw::math::Vector3D<> P2(-0.111626661713,0.366865480386,2.147308153); // <- left side of base-plate
+  std::cout<< "Distance from camera to base: " << Trans_camera_in_base.P().norm2() << std::endl;
 
-  auto P1_in_base = rw::math::inverse(Trans_camera_in_base)*P1;
+  auto P1_in_base = Trans_camera_in_base*P1;
+  auto P2_in_base = Trans_camera_in_base*P2;
+
+  std::cout << "P1 in camera: " <<  P1 << std::endl;
+  std::cout << "Distance from camera to P1: " << P1.norm2() << std::endl;
   std::cout << "P1 in base: " << P1_in_base << std::endl;
   std::cout << "Distance from base to P1: " << P1_in_base.norm2() << std::endl;
+
+  std::cout << "Distance between P1 and P2 in camera frame:" << (P1-P2).norm2() << std::endl;
+  std::cout << "Distance between P1 and P2 in base frame:" << (P1_in_base-P2_in_base).norm2() << std::endl;
+
   return 0;
 }
--- matlab/find_marker_cam.m
+++ matlab/find_marker_cam.m
@@ -2,12 +2,11 @@ clc; clear;
 
 data = csvread('~/hte_back.csv');
 
-
 robot_points = data(:,4:6);
 camera_points = data(:,1:3)/1000;
 
-%%
-scatter3(camera_points(:,1),camera_points(:,2),camera_points(:,3));
+%
+%scatter3(camera_points(:,1),camera_points(:,2),camera_points(:,3));
 %%
 clc; 
 assert( length(robot_points) == length(camera_points) );
@@ -21,14 +20,24 @@ assert( length(robot_points) == length(camera_points) );
  for i = 1:length(robot_points)
     H = H + (robot_points(i,:) - mu_base)'*(camera_points(i,:) - mu_camera);
  end
+
+[U,S,V] = svd(H);
+
+R = V*U';
+
+if det(R) < 0
+    R(:,3) = R(:,3)*-1;
+end
+
+P = - R*mu_base' + mu_camera';
+
+T=[R P; 0 0 0 1]
+
+norm(P)
  
- [U,S,V] = svd(H);
+%   Test with the point at the edge of the table
+%p1=[-0.317222072079;-0.487509135996;-1.85257493885; 1];
+p1 = [0.181131055917;0.37173324503;2.15828502449; 1];
+p2 = [-0.111626661713;0.366865480386;2.147308153; 1];
  
- R = V*U';
- 
- if det(R) < 0
-     R(:,3) = R(:,3)*-1;
- end
- 
- P = - R*mu_base' + mu_camera';
- norm(P)
+norm(T*p1)
